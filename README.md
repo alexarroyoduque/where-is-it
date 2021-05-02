e36ebbb6f4b5a059fe0d8cb317b9e90b2b1b93ca
AlexArroyoDuque <alexandrosAD90@gmail.com>
2021-05-02 18:57:58 +0200

    feat(firebase): new deploy

--- README.md
+++ README.md
@@ -4,17 +4,19 @@ This game weighs less than 1 kb.
 It's very simple. The game informs the target button. The player will click on that button.
 
 ## How to play
-You can download this repository and run dist/index.min.html or you can visit [online version](http://bit.ly/1qJkmP5)
+You can download this repository and run dist/index.min.html or you can visit [online version](https://where-is-it-online.web.app)
 
 ## For developers
 
-+ index.html with html code
++ index.html with redirect to game code with < 1kb
 
 + main.js with javascript code
 
 + gulpfile.js to generate minified files.
 
 ### Install dependencies
+
+Use node 6.*
 
 ```sh
 $ npm install
--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,38 @@
 var gulp = require('gulp');
+var replace = require('gulp-replace');
 var uglify = require('gulp-uglify');
 var inline = require('gulp-inline');
 var htmlmin = require('gulp-html-minifier');
 var rename = require('gulp-rename');
 var inject = require('gulp-inject-string');
 
-
 var paths = {
   app: 'app',
   dist: 'dist'
 };
 
-gulp.task('minifyIndex', function() {
+gulp.task('copyIndex', function() {
   gulp.src(paths.app + '/index.html')
+  .pipe(replace('game.html', 'game.min.html'))
+  .pipe(gulp.dest(paths.dist));
+})
+
+gulp.task('minifyGame', function() {
+  gulp.src(paths.app + '/game.html')
     .pipe(inline({
       js: uglify
     }))
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(rename('/index.min.html'))
+    .pipe(rename('/game.min.html'))
     .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('inject', function(){
-    gulp.src(paths.dist + '/index.min.html')
+    gulp.src(paths.dist + '/game.min.html')
         .pipe(inject.prepend('document.write(`'))
         .pipe(inject.append('`)'))
-        .pipe(rename('index.min.js'))
+        .pipe(rename('game.min.js'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['minifyIndex', 'inject'])
+gulp.task('default', ['minifyGame', 'copyIndex', 'inject'])
